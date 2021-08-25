19736d01640e7f03df0fadb7a1b5b98adce64086
Jiabin <TB-Terence@users.noreply.github.com>
2021-08-25 16:52:16 -0400

    Add `style` field for `Show Text` action

--- Runtime/Hotspot.cs
+++ Runtime/Hotspot.cs
@@ -4,41 +4,17 @@ using UnityEngine;
 
 namespace Treasured.SDK
 {
-    [DisallowMultipleComponent]
-    [ExecuteInEditMode]
     [AddComponentMenu("")]
-    public sealed class Hotspot : MonoBehaviour
+    public sealed class Hotspot : TObject
     {
         private static readonly Vector3 HotspotSize = Vector3.one * 0.3f;
 
         #region JSON Properties
         /// <summary>
-        /// The instance id of the object.
-        /// </summary>
-        [SerializeField]
-        [ReadOnly]
-        private string _id;
-        /// <summary>
-        /// The display for the object.
-        /// </summary>
-        [SerializeField]
-        private string _name;
-        /// <summary>
-        /// The description of the object.
-        /// </summary>
-        [SerializeField]
-        [TextArea(3, 3)]
-        private string _description;
-        /// <summary>
         /// Collider of the object.
         /// </summary>
         [SerializeField]
         private Hitbox _hitbox = new Hitbox();
-        /// <summary>
-        /// List of actions that will trigger when object is selected.
-        /// </summary>
-        [SerializeField]
-        private List<TreasuredAction> _onSelected = new List<TreasuredAction>();
         /// <summary>
         /// Visible objects from the hotspot. Used by hotspot.
         /// </summary>
@@ -49,32 +25,10 @@ namespace Treasured.SDK
 
         #region Editor Properties
         private Collider _collider;
-
-        public string Id
-        {
-            get
-            {
-                return _id;
-            }
-            private set
-            {
-                _id = value;
-            }
-        }
-        public string Name { get => _name; set => _name = value; }
-        public string Description { get => _description; set => _description = value; }
-        public Hitbox Hitbox { get => _hitbox; set => _hitbox = value; }
-        public List<TreasuredAction> OnSelected { get => _onSelected; set => _onSelected = value; }
-        public List<string> VisibleTargets { get => _visibleTargets; set => _visibleTargets = value; }
         #endregion
 
-        private void Awake()
-        {
-            if (string.IsNullOrEmpty(Id))
-            {
-                Id = Guid.NewGuid().ToString();
-            }
-        }
+        public Hitbox Hitbox { get => _hitbox; set => _hitbox = value; }
+        public List<string> VisibleTargets { get => _visibleTargets; set => _visibleTargets = value; }
 
         private void OnEnable()
         {
