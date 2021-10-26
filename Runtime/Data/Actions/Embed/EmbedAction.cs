5faff13794385de576701f86ed598b8468c575fb
Jiabin <TB-Terence@users.noreply.github.com>
2021-10-26 18:14:47 -0400

    Update EmbedAction structure

--- Runtime/Data/Actions/Embed/EmbedAction.cs
+++ Runtime/Data/Actions/Embed/EmbedAction.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Treasured.UnitySdk
 {
@@ -7,13 +6,7 @@ namespace Treasured.UnitySdk
     public abstract class EmbedAction : ActionBase
     {
         [SerializeField]
-        [TextArea(3, 3)]
-        private string _src;
-
-        [SerializeField]
         private EmbedPosition _position = EmbedPosition.TopRight;
-
-        public string Src { get => _src; set => _src = value; }
 
         public EmbedPosition Position { get => _position; set => _position = value; }
     }
