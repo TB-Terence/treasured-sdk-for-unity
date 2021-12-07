573829687fa8ad6f68b3f3377c0f59632d9cefac
Jiabin Tang <jiabintang@hotmail.com>
2021-12-06 21:55:51 -0500

    Add GLTF Plugins

---
+++ Editor/Export/MeshExportProcess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityGLTF;
+
+namespace Treasured.UnitySdk
+{
+    internal class MeshExportProcess : ExportProcess
+    {
+        public override void OnGUI(SerializedObject serializedObject)
+        {
+            
+        }
+
+        public override void Export(string rootDirectory, TreasuredMap map)
+        {
+            
+        }
+    }
+}
---
+++ Editor/UnityGLTF/Loader/FileLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System;
+using System.Threading.Tasks;
+
+namespace UnityGLTF.Loader
+{
+	public class FileLoader : IDataLoader, IDataLoader2
+	{
+		private readonly string _rootDirectoryPath;
+
+		public FileLoader(string rootDirectoryPath)
+		{
+			_rootDirectoryPath = rootDirectoryPath;
+		}
+
+		public Task<Stream> LoadStreamAsync(string relativeFilePath)
+		{
+			return Task.Run(() => LoadStream(relativeFilePath));
+		}
+
+		public Stream LoadStream(string relativeFilePath)
+		{
+			if (relativeFilePath == null)
+			{
+				throw new ArgumentNullException("relativeFilePath");
+			}
+
+			string pathToLoad = Path.Combine(_rootDirectoryPath, relativeFilePath);
+			if (!File.Exists(pathToLoad))
+			{
+				throw new FileNotFoundException("Buffer file not found", relativeFilePath);
+			}
+
+			return File.OpenRead(pathToLoad);
+		}
+	}
+}
