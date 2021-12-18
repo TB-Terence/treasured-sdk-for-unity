77a69ec7d876310f00ba1a6897d5b43885e0a2d9
Jiabin Tang <jiabintang@hotmail.com>
2021-12-17 22:33:16 -0500

    Add `new-tab` for embed position

--- Runtime/Data/Actions/Embed/EmbedPosition.cs
+++ Runtime/Data/Actions/Embed/EmbedPosition.cs
@@ -1,12 +1,17 @@
-﻿namespace Treasured.UnitySdk
+﻿using System;
+
+namespace Treasured.UnitySdk
 {
     public enum EmbedPosition
     {
         TopLeft,
         TopRight,
+        [Obsolete] // Keep the property there so that serializedProperty.enumValueIndex stays the same
         BottomLeft,
+        [Obsolete]
         BottomRight,
         Center,
-        Fullscreen
+        Fullscreen,
+        NewTab
     }
 }
