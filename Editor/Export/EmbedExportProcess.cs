08154bd65a88dd733aa0a9c5ae3bf1d403ef86c0
Jiabin Tang <jiabintang@hotmail.com>
2022-02-15 21:42:32 -0500

    Add HTML generator with template
    1. Add ability to load template.html from package resources folder
    2. Add ability to replace contents in the template.html file and generate a new file in the output folder

---
+++ Editor/Export/EmbedExportProcess.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace Treasured.UnitySdk
+{
+    internal class EmbedExportProcess : ExportProcess
+    {
+        private string output;
+
+        public override void OnGUI(SerializedObject serializedObject)
+        {
+            output = EditorGUILayout.TextArea(output);
+        }
+
+        public override void OnExport(string rootDirectory, TreasuredMap map)
+        {
+            string path = Path.Combine(rootDirectory, "embed.html");
+            HTMLGenerator.Generate(path, output);
+        }
+    }
+}
---
+++ Editor/Serialization/HTML/HTMLGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Treasured.UnitySdk
+{
+    internal class HTMLGenerator
+    {
+        public const string TemplateFileRelativePath = "Packages/com.treasured.unitysdk/Resources/template.html";
+        
+        private static Regex s_regex = new Regex(@"([^A-z0-9])(#CODE#){1}([^A-z0-9])");
+
+        public static void Generate(string outputPath, string replacement)
+        {
+            string templatePath = Path.GetFullPath(TemplateFileRelativePath);
+            if (!File.Exists(templatePath))
+            {
+                throw new FileNotFoundException("HTML template file not found.");
+            }
+            string template = File.ReadAllText(templatePath);
+            string result = s_regex.Replace(template, $"$1{replacement}$3");
+            File.WriteAllText(outputPath, result);
+        }
+    }
+}
