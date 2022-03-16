2444a1e9ee3b4dfa22b7f9242ffa24b743225249
Jiabin Tang <jiabintang@hotmail.com>
2022-03-15 21:53:46 -0400

    Remove Hitbox Gizmos

--- Runtime/Components/Hitbox.cs
+++ Runtime/Components/Hitbox.cs
@@ -8,23 +8,23 @@ namespace Treasured.UnitySdk
 #if UNITY_EDITOR
         void OnDrawGizmosSelected()
         {
-            Color tempColor = Gizmos.color;
-            Matrix4x4 tempMatrix = Gizmos.matrix;
-            Gizmos.matrix = Matrix4x4.TRS(this.transform.position, this.transform.rotation, Vector3.one);
+            //Color tempColor = Gizmos.color;
+            //Matrix4x4 tempMatrix = Gizmos.matrix;
+            //Gizmos.matrix = Matrix4x4.TRS(this.transform.position, this.transform.rotation, Vector3.one);
 
-            if (transform.parent != null && transform.parent.GetComponent<Hotspot>())
-            {
-                Gizmos.color = Color.red;
-            }
-            else
-            {
-                //Gizmos.color = TreasuredSDKSettings.Instance ? TreasuredSDKSettings.Instance.hitboxColor : TreasuredSDKSettings.defaultHitboxColor;
-            }
+            //if (transform.parent != null && transform.parent.GetComponent<Hotspot>())
+            //{
+            //    Gizmos.color = Color.red;
+            //}
+            //else
+            //{
+            //    //Gizmos.color = TreasuredSDKSettings.Instance ? TreasuredSDKSettings.Instance.hitboxColor : TreasuredSDKSettings.defaultHitboxColor;
+            //}
 
-            Gizmos.DrawCube(Vector3.zero, transform.localScale);
+            //Gizmos.DrawCube(Vector3.zero, transform.localScale);
 
-            Gizmos.color = tempColor;
-            Gizmos.matrix = tempMatrix;
+            //Gizmos.color = tempColor;
+            //Gizmos.matrix = tempMatrix;
         }
 #endif
     }
