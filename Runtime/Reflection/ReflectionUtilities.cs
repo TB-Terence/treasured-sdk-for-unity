657fd3b24189a54c784be2a36a3d2bdb40069c09
Jiabin Tang <jiabintang@hotmail.com>
2022-04-09 12:45:55 -0400

    Add utility to get serialized fields with attribute

---
+++ Runtime/Reflection/ReflectionUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Treasured.UnitySdk
+{
+    public static class ReflectionUtilities
+    {
+        public static FieldInfo[] GetSeriliazedFields(object target)
+        {
+            Type type = target.GetType();
+            FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            return fieldInfos.Where(fieldInfo => fieldInfo.IsPublic || fieldInfo.IsDefined(typeof(UnityEngine.SerializeField))).ToArray();
+        }
+
+        public static FieldInfo[] GetSeriliazedFieldsWithAttribute<T>(object target) where T : Attribute
+        {
+            FieldInfo[] fieldInfos = GetSeriliazedFields(target);
+            return fieldInfos.Where(fieldInfo => fieldInfo.IsDefined(typeof(T))).ToArray();
+        }
+    }
+}
