b0ff76e4e52de6d751f7c0fcf983dac0f2e57024
Jiabin Tang <jiabintang@hotmail.com>
2022-05-26 16:29:02 -0400

    feat: add set background music action

---
+++ Runtime/Actions/SetBackgroundMusic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Treasured.UnitySdk
+{
+    public sealed class SetBackgroundMusic : Action
+    {
+        [TextArea(3, 8)]
+        public string src;
+        [Range(0, 100)]
+        public int volume = 50;
+    }
+}
