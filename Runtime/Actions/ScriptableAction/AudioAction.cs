e36d4f4aeab29a4454e81e0a68c9122ac18a895d
Jiabin Tang <jiabintang@hotmail.com>
2022-07-11 20:49:29 -0400

    feat: add audio action

---
+++ Runtime/Actions/ScriptableAction/AudioAction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Treasured.UnitySdk
+{
+    [API("playAudio")]
+    public class AudioAction : ScriptableAction
+    {
+        [Url]
+        public string src;
+
+        [Range(0, 100)]
+        public int volume = 100;
+
+        public bool loop = false;
+    }
+}
