babfef68a483d33d33f2209fd99d0b7020f273ca
Jiabin Tang <jiabintang@hotmail.com>
2022-07-11 22:38:38 -0400

    feat: add pan action

---
+++ Runtime/Actions/ScriptableAction/PanAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Treasured.UnitySdk
+{
+    [API("pan")]
+    public class PanAction : ScriptableAction
+    {
+        public float amount = 0.1f;
+        [Min(1)]
+        public int duration = 1000;
+
+        public override object[] GetArguments()
+        {
+            return new object[] { amount, duration };
+        }
+    }
+}
