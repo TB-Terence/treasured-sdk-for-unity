d71677d77707793a105b3c675b1647618b5384f8
Jiabin Tang <12288368+TB-Terence@users.noreply.github.com>
2022-08-09 21:45:25 -0400

    fixed action outputs (#77)

--- Runtime/Json/Converters/GoToActionConverter.cs
+++ Runtime/Json/Converters/GoToActionConverter.cs
@@ -30,8 +30,6 @@ namespace Treasured.UnitySdk
                     writer.WriteStartObject();
                     writer.WritePropertyName("id");
                     writer.WriteValue(action.Id);
-                    writer.WritePropertyName("type");
-                    writer.WriteValue(action.Type);
                     writer.WritePropertyName("hotspotId");
                     writer.WriteValue(action.target.Id);
                     writer.WritePropertyName("message");
