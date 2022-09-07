52b91f59cda506b829ca68f54ed361937754d7a1
Jiabin Tang <jiabintang@hotmail.com>
2022-09-06 22:03:00 -0400

    fix: mixed toggle state and more
    1. fixed mixed toggle state for export actions
    2. add description and previewUrl
    3. optimize toggle state for object list
    4. make url attribute to be render inline
    5. fixed text duration issue

--- Editor/Drawers/ActionListDrawer.cs
+++ Editor/Drawers/ActionListDrawer.cs
@@ -11,11 +11,14 @@ namespace Treasured.UnitySdk
         internal ReorderableList reorderableList;
         public string Header { get; set; }
 
-        private bool enabled = false;
+        private bool _enabledAll = false;
+
+        private TreasuredMapEditor.GroupToggleState _toggleState;
 
         public ActionListDrawer(SerializedObject serializedObject, SerializedProperty elements, string header)
         {
             Header = header;
+            UpdateToggleState(elements);
             reorderableList = new ReorderableList(serializedObject, elements)
             {
                 drawHeaderCallback = (Rect rect) =>
@@ -23,15 +26,17 @@ namespace Treasured.UnitySdk
                     if (typeof(ScriptableAction).IsAssignableFrom(typeof(T))) // TODO: Remove this after migrate to GuidedTourV2
                     {
                         EditorGUI.BeginChangeCheck();
-                        enabled = EditorGUI.ToggleLeft(rect, new GUIContent(Header, $"{(enabled ? "Disable" : "Enable")} all"), enabled);
+                        EditorGUI.showMixedValue = _toggleState == TreasuredMapEditor.GroupToggleState.Mixed;
+                        _enabledAll = EditorGUI.ToggleLeft(rect, new GUIContent(Header, $"{(_enabledAll ? "Disable" : "Enable")} all"), _enabledAll);
                         if (EditorGUI.EndChangeCheck())
                         {
                             for (int i = 0; i < elements.arraySize; i++)
                             {
                                 SerializedProperty element = elements.GetArrayElementAtIndex(i);
                                 SerializedProperty enabledProperty = element.FindPropertyRelative("enabled");
-                                enabledProperty.boolValue = enabled;
+                                enabledProperty.boolValue = _enabledAll;
                             }
+                            UpdateToggleState(elements);
                         }
                     }
                     else
@@ -57,7 +62,12 @@ namespace Treasured.UnitySdk
                         {
                             Rect buttonRect = new Rect(rect.x, rect.y, 25, EditorGUIUtility.singleLineHeight);
                             SerializedProperty enabled = element.FindPropertyRelative("enabled");
+                            EditorGUI.BeginChangeCheck();
                             enabled.boolValue = EditorGUI.ToggleLeft(buttonRect, new GUIContent(ObjectNames.NicifyVariableName(name)), enabled.boolValue);
+                            if (EditorGUI.EndChangeCheck())
+                            {
+                                UpdateToggleState(elements);
+                            }
                             EditorGUI.PropertyField(new Rect(rect.x + 25, rect.y, rect.width - 25, rect.height), element, new GUIContent(ObjectNames.NicifyVariableName(name)), true);
                         }
                         else
@@ -113,5 +123,26 @@ namespace Treasured.UnitySdk
             reorderableList.serializedProperty.serializedObject.ApplyModifiedProperties();
         }
 
+        private void UpdateToggleState(SerializedProperty elements)
+        {
+            bool[] enabled = new bool[elements.arraySize];
+            for (int i = 0; i < elements.arraySize; i++)
+            {
+                SerializedProperty element = elements.GetArrayElementAtIndex(i);
+                SerializedProperty enabledProperty = element.FindPropertyRelative("enabled");
+                enabled[i] = enabledProperty.boolValue;
+            }
+            int enabledCount = enabled.Count(x => x == true);
+            if (enabledCount == enabled.Length)
+            {
+                _toggleState = TreasuredMapEditor.GroupToggleState.All;
+                _enabledAll = true;
+            }
+            else
+            {
+                _toggleState = enabledCount == 0 ? TreasuredMapEditor.GroupToggleState.None : TreasuredMapEditor.GroupToggleState.Mixed;
+                _enabledAll = false;
+            }
+        }
     }
 }
