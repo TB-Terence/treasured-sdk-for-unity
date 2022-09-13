9224d9a37e79a5312f1cdf7e3d6f233a978c08d6
Jiabin Tang <jiabintang@hotmail.com>
2022-09-13 19:45:52 -0400

    fix: process not associated with this object

--- Editor/TreasuredMapEditor.cs
+++ Editor/TreasuredMapEditor.cs
@@ -410,9 +410,10 @@ namespace Treasured.UnitySdk
                                     _npmProcess.StartInfo.RedirectStandardOutput = true;
                                     _npmProcess.StartInfo.WorkingDirectory = TreasuredSDKPreferences.Instance.customExportFolder;
 
-                                    _map.processId = _npmProcess.Id;
 
                                     _npmProcess.Start();
+
+                                    _map.processId = _npmProcess.Id;
 
                                     UnityEditor.EditorApplication.quitting -= KillProcess;
                                     UnityEditor.EditorApplication.quitting += KillProcess;
