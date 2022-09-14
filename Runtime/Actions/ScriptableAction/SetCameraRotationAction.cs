9ffe5094d8dec1013aabccfb4e348984d8682739
Jiabin Tang <jiabintang@hotmail.com>
2022-09-13 20:51:21 -0400

    feat: add lower speed factor settings

--- Runtime/Actions/ScriptableAction/SetCameraRotationAction.cs
+++ Runtime/Actions/ScriptableAction/SetCameraRotationAction.cs
@@ -1,12 +1,33 @@
-﻿using UnityEngine;
+﻿using Newtonsoft.Json;
+using System;
+using UnityEngine;
 
 namespace Treasured.UnitySdk
 {
     [API("setCameraRotation")]
     public class SetCameraRotationAction : ScriptableAction
     {
+        public enum Speed
+        {
+            x0_25,
+            x0_5,
+            x0_75,
+            x1,
+            x1_25,
+            x1_5,
+            x1_75,
+            x2
+        }
         public Quaternion rotation;
-        [Range(1, 10)]
-        public int speedFactor = 1;
+        [JsonIgnore]
+        public Speed speed = Speed.x1;
+
+        public float SpeedFactor
+        {
+            get
+            {
+                return (Array.IndexOf(Enum.GetValues(typeof(Speed)), speed) + 1) * 0.25f;
+            }
+        }
     }
 }
