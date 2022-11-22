32a28960c8101ce7de8425f073ce82bea5ea0d87
Jiabin Tang <jiabintang@hotmail.com>
2022-11-21 20:54:19 -0500

    Update embed action outputs

--- Runtime/Actions/ScriptableAction/EmbedAction.cs
+++ Runtime/Actions/ScriptableAction/EmbedAction.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
 namespace Treasured.UnitySdk
 {
@@ -6,12 +9,45 @@ namespace Treasured.UnitySdk
     public class EmbedAction : ScriptableAction
     {
         [Url]
+        [JsonIgnore]
         public string src;
+        [JsonProperty("src")]
+        public string Src
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(code))
+                {
+                    return src;
+                }
+                var doc = new HtmlDocument();
+                doc.LoadHtml(code);
+                var iframe = doc.DocumentNode.SelectSingleNode("//iframe");
+                return iframe == null ? "" : iframe.GetAttributeValue("src", "");
+            }
+        }
         public string title;
         [TextArea(3, 5)]
         public string description;
         [Url]
         public string previewUrl;
         public EmbedPosition position = EmbedPosition.Fullscreen;
+        [TextArea(3, 5)]
+        [JsonIgnore]
+        public string code;
+        [JsonProperty("code")]
+        public string Code
+        {
+            get
+            {
+                var doc = new HtmlDocument();
+                doc.LoadHtml(code);
+                doc.DocumentNode?.FirstChild?.SetAttributeValue("display", "block");
+                doc.DocumentNode?.FirstChild?.SetAttributeValue("width", "100%");
+                Debug.LogError(doc.DocumentNode.OuterHtml);
+                return doc.DocumentNode.OuterHtml;
+            }
+        }
+        public string[] scripts = new string[0];
     }
 }
