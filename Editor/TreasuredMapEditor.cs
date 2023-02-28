12d8193dcb767870018e8cccedb494abd630a190
Jalpen <djalpen24@gmail.com>
2023-02-28 11:04:11 -0500

    Fixed issue with single export

--- Editor/TreasuredMapEditor.cs
+++ Editor/TreasuredMapEditor.cs
@@ -473,7 +473,67 @@ namespace Treasured.UnitySdk
                         try
                         {
                             Exporter.Export(_map);
-                            // EditorUtility.DisplayDialog("Export Finished", $"Scene export finished.", "OK");
+
+                            var exporters = ReflectionUtils.GetSerializedFieldValuesOfType<Exporter>(_map);
+                            if (exporters.Any(result => (result is CubemapExporter || result is MeshExporter) && result.enabled))
+                            {
+                                string argument;
+                                
+                                if (_map.exportSettings.optimizeScene
+                                    || _map.exportSettings.ExportType == ExportType.ProductionExport)
+                                {
+                                    argument =
+                                        $"treasured optimize \"{_map.exportSettings.OutputDirectory}\"";
+                                }
+                                else
+                                {
+                                    argument =
+                                        $"treasured optimize \"{_map.exportSettings.OutputDirectory}\" --skipGlb";
+                                }
+                                
+                                // Run `treasured optimize` to optimize the glb file
+                                var npmProcess = ProcessUtilities.CreateProcess(argument);
+
+                                string stdOutput = "";
+                                try
+                                {
+                                    npmProcess.Start();
+
+                                    while (!npmProcess.HasExited)
+                                    {
+                                        if (UnityEditor.EditorUtility.DisplayCancelableProgressBar("Finalizing Export",
+                                            $"Please wait. Processing {_map.exportSettings.folderName}...", 50 / 100f))
+                                        {
+                                            ProcessUtilities.KillProcess(npmProcess);
+                                            throw new OperationCanceledException();
+                                        }
+                                    }
+
+                                    stdOutput = npmProcess.StandardOutput.ReadToEnd();
+
+                                    // EditorUtility.DisplayDialog("Export Finished", $"Scene export finished.", "OK");
+                                }
+                                catch (OperationCanceledException e)
+                                {
+                                    EditorUtility.DisplayDialog("Canceled", e.Message, "OK");
+                                }
+                                catch (Exception e)
+                                {
+                                    throw new ApplicationException(e.Message);
+                                }
+                                finally
+                                {
+                                    if (!string.IsNullOrEmpty(stdOutput))
+                                    {
+                                        Debug.Log(stdOutput);
+                                    }
+
+                                    npmProcess?.Dispose();
+                                    EditorUtility.ClearProgressBar();
+                                }
+                            }
+                            
+                            EditorUtility.DisplayDialog("Export Finished", $"Scene export finished.", "OK");
                         }
                         catch (ValidationException e)
                         {
